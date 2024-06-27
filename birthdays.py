b12591fc46b5505aab09e68f59b3f671b0b7b4db
Aman Kumar Das <amankumardas109@gmail.com>
2024-06-27 12:33:04 +0530

    birthdays.py updated

--- birthdays.py
+++ birthdays.py
@@ -1,4 +1,7 @@
-# Explore possibilities of birthday paradox
+'''
+Simulating 100000 times for how many peoples can have same birthdays for N people
+'''
+
 import datetime, random
 
 def getBirthdays(NumBday):
@@ -21,9 +24,15 @@ def getMatch(birthdays):
         for j, birthday2 in enumerate(birthdays[i+1:]):
             if birthday1 == birthday2:
                 return birthday1
+            
+def formatDay(birthdays):
+    #Format day into DD MMM format 
+    return [birthday.strftime('%b %d') for birthday in birthdays]
     
-MONTHS = ('Jan', 'Feb', "Mar", "Apr", "May", 'June','July', 'Aug', 'Sept','Oct', 'Nov','Dec')
 
+# MONTHS = ('Jan', 'Feb', "Mar", "Apr", "May", 'June', 'July', 'Aug', 'Sept','Oct', 'Nov','Dec')
+
+#loop till you enter a correct number
 while True:
     print('How many Birthdays Should i generate: (Max 100)')
     response = input('>> ')
@@ -35,17 +44,21 @@ print('Here are {} bithdays:'.format(numBday))
 birthdays = getBirthdays(numBday)
 
 # print(birthdays)
+# #display birthdays right
+# for i, birthday in enumerate(birthdays):
+#     if i != 0:
+#         print(',', end = ' ')
+#     month = MONTHS[birthday.month - 1]
+#     dateText = '{} {}'.format(month, birthday.day)
+#     print(dateText, end=' ')
 
-#display birthdays right
-for i, birthday in enumerate(birthdays):
-    if i != 0:
-        print(',', end = ' ')
-    month = MONTHS[birthday.month - 1]
-    dateText = '{} {}'.format(month, birthday.day)
-    print(dateText, end=' ')
-
+#display birthdays
+formated_day = formatDay(birthdays)
+for day in formated_day:
+    print(day, end='  ')
 print()
 print()
+
 
 #matching the birthday
 match = getMatch(birthdays)
@@ -53,18 +66,19 @@ match = getMatch(birthdays)
 #displaying of match
 print('In this simulation : ')
 if match != None:
-    birthMonth = MONTHS[match.month - 1]
-    dateText2 = '{} {}'.format(birthMonth, match.day)
-    print('Multiple people have matched birthday on day {}'.format(dateText2))
+    # birthMonth = MONTHS[match.month - 1]
+    # dateText2 = '{} {}'.format(birthMonth, match.day)
+    matchDay = match.strftime('%b %d')
+    print('Multiple people have matched birthday on day {}'.format(matchDay))
 else:
     print("There are no matchig birthdays")
-
 print()
 print()
 
-#Doing this 100_000 times 
-print('Generating', numBday, '100000 times')
-input('Press Any key to continue : ')
+
+#Now Doing this 100_000 times 
+print('Generating', numBday, 'bithdays, 100000 times')
+input('Press Enter to continue : ')
 
 simMatch = 0
 for i in range(100000):
@@ -78,20 +92,5 @@ print('100000 simulation run done')
 
 probablity = round(simMatch/100000 *100, 2)
 print('So there are ', probablity, '% chance that', numBday, 'people, will have same bithdays')
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
+print()
 
